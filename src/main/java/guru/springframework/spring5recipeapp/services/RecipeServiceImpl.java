cd1ac465e0366a4634160733c4380511d2b3d527
Edu <garcia_eduher@gva.es>
2022-10-24 17:49:16 +0200

    Display recipe by id

--- src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5recipeapp.services;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
@@ -24,5 +25,17 @@ public class RecipeServiceImpl implements RecipeService {
     	Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+    
+    @Override
+    public Recipe findById(Long l) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return recipeOptional.get();
     }
 }
