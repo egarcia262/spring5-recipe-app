5f7155c2cdd2e91d1c890f0b9e1b6ae9639655cb
Edu <garcia_eduher@gva.es>
2022-10-26 17:07:27 +0200

    Correct test error

--- src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring5recipeapp.services;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -8,27 +8,34 @@ import static org.mockito.Mockito.when;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import guru.springframework.spring5recipeapp.converters.RecipeCommandToRecipe;
+import guru.springframework.spring5recipeapp.converters.RecipeToRecipeCommand;
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class RecipeServiceImplTest {
 
 	RecipeServiceImpl recipeService;
-
+	 
     @Mock
     RecipeRepository recipeRepository;
+    
+    @Mock
+    RecipeToRecipeCommand recipeToRecipeCommand;
 
-
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.openMocks(this);
-
-        recipeService = new RecipeServiceImpl(recipeRepository);
+    @Mock
+    RecipeCommandToRecipe recipeCommandToRecipe;
+    
+    @BeforeEach
+    void setUp() {
+        recipeService = new RecipeServiceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
     }
 
     @Test
